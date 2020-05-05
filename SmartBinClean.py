48497cd43b3bac94981cb38ccc03ea63a71cb74c
SchmidtRichard <ri.schmidta@gmail.com>
2020-05-05 01:45:17 +0100

    removing unnecessary code in python

--- SmartBinClean.py
+++ SmartBinClean.py
@@ -2,7 +2,6 @@
 # BSc (Honours) in Computing - IoT Stream
 # GrovePi + Grove Ultrasonic Ranger + Relay Pin
 
-
 from grovepi import *
 
 # Connect the Grove Ultrasonic Ranger to digital port D4
@@ -14,12 +13,11 @@ Relay_pin = 2#NECESSARIO?
 
 pinMode(Relay_pin,"OUTPUT")
 
-
 time_to_sleep       = 1       # The main loop runs every 1.5 seconds
 
 while True:
     try:
-
+        
         # Read distance value from Ultrasonic
         distant = ultrasonicRead(ultrasonic_ranger)
 
@@ -36,7 +34,7 @@ while True:
             print('The bin is half-full.')
             print(distant,'cm available')
             digitalWrite(Relay_pin,1)
-        
+
         if distant >= 20 and distant < 10:
             print('______________________________')
             print('Almost full')
@@ -57,44 +55,5 @@ while True:
     except IOError:
         print("Error")
 
-
     #Slow down the loop
     time.sleep(time_to_sleep)
-
-
-
-
-
-
-
-        # Read distance value from Ultrasonic
-        #distant = ultrasonicRead(ultrasonic_ranger)
-
-	#print(distant,'cm - empty')
-
-
-        #if distant > 25:
-            #digitalWrite(Relay_pin,1)
-
-
-        #print(distant,'cm')
-
-	#print('The bin is full. Please collect it as there is only ', distant,'cm available')
-        #elif distant <= 10:
-            #digitalWrite(Relay_pin,1)
-	    
-        #else:
-            #digitalWrite(Relay_pin,0)
-
-
-
-
-
-
-
-
-
-
-
-
-
