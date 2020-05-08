131a8048d02508683a48bdc6f148d694b21656e5
SchmidtRichard <ri.schmidta@gmail.com>
2020-05-09 00:13:34 +0100

    monitoring_admin.php - Bootstrap glyphicon fix, titles fix, code tide up and more comments added

--- admin/monitoring_admin.php
+++ admin/monitoring_admin.php
@@ -1,5 +1,13 @@
+<!--
+Richard Schmidt de Almeida
+National College of Ireland
+Bsc (Honours) in Computing - IoT Stream
+Software Project - May 2020
+Smart BinClean Project
+-->
+
 <?php
-//This for the admin session, create a new session which will carry the infor of the admin authentication
+//This is for the admin session, create a new session which will carry the info of the admin authentication - brownser cache
 session_start();//Takes the live session from the login.php
 
 ?>
@@ -87,12 +95,13 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 				</a>
 
 				<div class="pull-right"><a href="/admin/user_registration/logout.php"><!-- Redicrects to logout -->
-					<i class="fa fa-fw fa-user; glyphicon glyphicon-log-out"></i>Logout</b>
+					<i class="fa fa-fw fa-user; glyphicon glyphicon-log-out"></i> Logout</b>
 				</a></div>
 
 
+
 					<!-- Button to Open the Modal to add the Bin -->
-					  <button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal1">Add bin</button>
+					  <button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal1">Add Bin</button>
 
 					  <!-- The Modal -->
 					  <div class="modal" id="myModal1">
@@ -126,13 +135,14 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 						</div>
 					  </div>
 
-					  <!-- Button to Open the Modal -->
-					  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal2">Edit bin</button>
 
+
+					  <!-- Button to Open the Modal to Edit Bin -->
+					  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal2">Edit Bin</button>
 					  <!-- The Modal -->
 					  <div class="modal" id="myModal2">
 						<div class="modal-dialog">
-						  <div class="modal-content">
+						<div class="modal-content">
 
 							<!-- Modal Header Title -->
 							<div class="modal-header">
@@ -143,7 +153,10 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 							<!-- Modal body -->
 							<div class="modal-body">
 
-								  <form action="/admin/update_bin_info_from_web_admin.php" method="post"><!-- Takes the info inserted and send through update_bin_info_from_web_admin.php to then be stored into the DB -->
+
+									<!-- Takes the info inserted and send through update_bin_info_from_web_admin.php to then be stored into the DB -->
+								  <form action="/admin/update_bin_info_from_web_admin.php" method="post">
+
 												ID: <br><input type="text" name="id"><br><br>
 												Bin type:
 												  <select id="type" name="type">
@@ -155,14 +168,13 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 												<br><br><input type="submit" name="reg" value="Update the database!"/>
 								  </form>
 							</div>
-
 						  </div>
 						</div>
 					  </div>
 
 
-					    <!-- Button to Open the Modal -->
-				  <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal3">Del bin</button>
+					<!-- Button to Open the Modal - Delete Bin -->
+				  <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#myModal3">Del Bin</button>
 
 				  <!-- The Modal -->
 				  <div class="modal" id="myModal3">
@@ -177,10 +189,14 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 
 					<!-- Modal body -->
 					<div class="modal-body">
-						<form action="/admin/delete_from_web_admin.php" method="post"><!-- Takes the info inserted and send through delete_from_web_admin.php to then be stored into the DB -->
+
+
+						<!-- Takes the info inserted and send through delete_from_web_admin.php to then be stored into the DB -->
+						<form action="/admin/delete_from_web_admin.php" method="post">
+
 							  ID: <br><input type="text" name="id">
 
-							  <br><br><input type="submit" name="reg" value="Submit"/>
+							  <br><br><input type="submit" name="reg" value="Delete Bin"/>
 						</form>
 					</div>
 
@@ -188,8 +204,10 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 				  </div>
 				  </div>
 
-				     <!-- Button to Open the Modal -->
-				  <button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal4">Solve a reported issue</button>
+
+
+				  <!-- Button to Open the Modal - Report a Issue -->
+				  <button type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal4">Solve a Reported Issue</button>
 
 				  <!-- The Modal -->
 				  <div class="modal" id="myModal4">
@@ -204,10 +222,15 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 
 					<!-- Modal body -->
 					<div class="modal-body">
-						<form action="/admin/solve_a_reported_issue_from_web_admin.php" method="post"><!-- Takes the info inserted and send through solve_a_reported_issue_from_web_admin.php to then be stored into the DB -->
+
+
+						<!-- Takes the info inserted and send through solve_a_reported_issue_from_web_admin.php to then be stored into the DB -->
+						<form action="/admin/solve_a_reported_issue_from_web_admin.php" method="post">
+
+
 							  ID: <br><input type="text" name="id">
 							  <br><br>Solve an issue? Yes: <input type="checkbox" id="issue" name="issue" value="yes">   No:  <input type="checkbox" id="issue" name="issue" value="no">
-							  <br><br><input type="submit" name="reg" value="Submit"/>
+							  <br><br><input type="submit" name="reg" value="Submit Report"/>
 						</form>
 					</div>
 
@@ -215,11 +238,8 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 				  </div>
 				  </div>
 
-
-
-
-			  <!-- Button to Open the Modal -->
-			  <div class="pull-right"><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal">Delete existing user</button></div>
+			  <!-- Button to Open the Modal - Delete / List Users -->
+			  <div class="pull-right"><button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal">Delete Existing User</button></div>
 
 
 			  <!-- The Modal -->
@@ -229,12 +249,20 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 
 					<!-- Modal body -->
 					<div class="modal-body">
+
+									<!-- Takes the info inserted and send through list_users_from_db.php  -->
 					  			<form action="/admin/user_registration/list_users_from_db.php"><input type="submit" value="List all users created if you forget the username" /></form>
+
 								<h3 align="left">Delete user form:</h3>
-								<form action="/admin/user_registration/delete_user_from_db.php" method="post"><!-- Sends to delete_user_from_db.php -->
+
+
+								<!-- Sends to delete_user_from_db.php -->
+								<form action="/admin/user_registration/delete_user_from_db.php" method="post">
+
+
 								   <br><br>Username:<br><input type="text" name="username">
 								   <br><br>Email:<br><input type="text" name="email" >
-								<br><br><input type="submit" name="reg" value="Delete user!" />
+								<br><br><input type="submit" name="reg" value="Delete User!" />
 								</form>
 					</div>
 					<div class="modal-footer">
@@ -251,16 +279,17 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 				<div class="modal-dialog">
 				  <div class="modal-content">
 
+
 					    <a href="/admin/user_registration/list_users_from_db.php" class="btn btn-info" role="button">List all users created</a>
 
 				  </div>
 				</div>
-			  </div>	-->
+			  </div>
 
 
 
-			  <!-- Button to Open the Modal -->
-			  <div class="pull-right"><button type="button" class="btn btn-success" data-toggle="modal" data-target="#regModal">Register new user</button></div>
+			  <!-- Button to Open the Modal - Register New User -->
+			  <div class="pull-right"><button type="button" class="btn btn-success" data-toggle="modal" data-target="#regModal">Register New User</button></div>
 
 			  <!-- The Modal -->
 			  <div class="modal" id="regModal">
@@ -285,10 +314,6 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 				  </div>
 				</div>
 			  </div>
-
-
-
-
 		</div>
 		<h1 align="center">
 			 <font size="6">City Center Bins</font>
@@ -301,7 +326,6 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 	$username = "root";
 	$password = "";
 	$dbname = "bin";
-
 
 		// Open connectivity between PHP and DB
 		$conn = new mysqli($servername, $username, $password, $dbname);
@@ -319,14 +343,14 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 		echo '<table border="0" width="80%">';
 
 		$count = 0;
-
 		//Gets the $result and check if not 0, then interate through the records into the db (id, image, location, date)
 		if ($result->num_rows > 0) {
+
 			// output data of each row
 			while($row = $result->fetch_assoc()) {//fecthing each interaction in the DB
 				$count++;
 
-		  //Put int each variable the info taken from the DB for each index number
+		  //Put into each variable the info taken from the DB for each index number
 				$id=$row["id"];
 				$type=$row["type"];
 				$img=$row["image"];
@@ -361,12 +385,11 @@ if (($_SESSION)&&($_SESSION['youarelogged'])==FALSE)//Check if the session has b
 			}
 		echo "</div></table>";
 		$conn->close();//Close connectivity between PHP and DB
-
 	}
 }
 else
 {
-	echo ('<h1>You\'re not the ADMIN</h1>');
+	echo ('<h1>You\'re not the ADMIN!</h1>');
 	echo '<meta http-equiv="refresh" content="2; URL=/admin/user_registration/logout.php"/>'; //Logs user out when trying to access a not authorized URL
 }
 ?>
