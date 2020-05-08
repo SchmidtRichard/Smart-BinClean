ae39d8935a895d666208fbe0e72ea024337db21a
SchmidtRichard <ri.schmidta@gmail.com>
2020-05-09 00:55:51 +0100

    monitoring.php - JS timedRefresh function amended, code tide up and more comments added

--- users/monitoring.php
+++ users/monitoring.php
@@ -1,5 +1,13 @@
+<!--
+Richard Schmidt de Almeida
+National College of Ireland
+Bsc (Honours) in Computing - IoT Stream
+Software Project - May 2020
+Smart BinClean Project
+-->
+
 <?php
-////This for the user session, create a new session which will carry the info of the user authentication
+//This is for the user session, create a new session which will carry the info of the user authentication
 session_start();
 
 ?>
@@ -7,7 +15,7 @@ session_start();
 
 if ($_SESSION) //Check if the session has been created previously in login.php
 {
-	if (($_SESSION['youarelogged'])==TRUE) //Check if it is an user
+	if (($_SESSION['youarelogged'])==TRUE) //Check if it is a standard user
 	{
 		echo'
 
@@ -30,14 +38,14 @@ if ($_SESSION) //Check if the session has been created previously in login.php
 			body {
 				font-family: Arial, Helvetica, sans-serif;
 			}
-			
+
 			.navbar {
 				width: 100%;
 				text-align: center;
 				background-color: #555;
 				overflow: auto;
 			}
-			
+
 			.navbar a {
 				float: left;
 				padding: 12px;
@@ -65,7 +73,7 @@ if ($_SESSION) //Check if the session has been created previously in login.php
 				flex: 50%;
 				padding: 0 4px;
 				padding-left: 30px;
-			}	
+			}
 
 		</style>
 
@@ -74,28 +82,30 @@ if ($_SESSION) //Check if the session has been created previously in login.php
 
     <div class="navbar"><!-- Create the nav on the top -->
 			<a class="active" href="/users/monitoring.php">
-				<i class="fa fa-fw fa-home; glyphicon glyphicon-home"></i><b>Home</b>
+				<i class="fa fa-fw fa-home; glyphicon glyphicon-home"></i><b> Home</b>
 			</a>
 
 			<a href="/users/index_empty.php"><!-- Redicrects to empty -->
-				<i class="fa fa-fw fa-search; glyphicon glyphicon-trash"></i>Empty</b>
+				<i class="fa fa-fw fa-search; glyphicon glyphicon-trash"></i> Empty</b>
 			</a>
 
 			<a href="/users/index_half.php"><!-- Redicrects to half full -->
-				<i class="fa fa-fw fa-envelope; glyphicon glyphicon-trash"></i>Half-Full</b>
+				<i class="fa fa-fw fa-envelope; glyphicon glyphicon-trash"></i> Half-Full</b>
 			</a>
 
 			<a href="/users/index_full.php"><!-- Redicrects to full -->
-				<i class="fa fa-fw fa-user; glyphicon glyphicon-trash"></i>Full</b>
+				<i class="fa fa-fw fa-user; glyphicon glyphicon-trash"></i> Full</b>
 			</a>
-			
+
 			<div class="pull-right"><a href="/admin/user_registration/logout.php"><!-- Redicrects to logout -->
-					<i class="fa fa-fw fa-user; glyphicon glyphicon-log-out"></i>Logout</b>
+					<i class="fa fa-fw fa-user; glyphicon glyphicon-log-out"></i> Logout</b>
 				</a>
 			</div>
-		
+
+
+
 		<!-- Button to Open the Modal -->
-			  <div class="pull-right"><button type="button" class="btn btn-info" data-toggle="modal" data-target="#regModal">Report an issue!</button></div>
+			  <div class="pull-right"><button type="button" class="btn btn-info" data-toggle="modal" data-target="#regModal">Report an Issue!</button></div>
 
 			  <!-- The Modal -->
 			  <div class="modal" id="regModal">
@@ -110,12 +120,12 @@ if ($_SESSION) //Check if the session has been created previously in login.php
 							  <br><br><input type="submit" name="reg" value="Submit!"/>
 						</form>
 					</div>
-						 
+
 				  </div>
 				</div>
 			  </div>
-		
-		
+
+
     </div>
     <h1 align="center">
          <font size="6">City Center Bins</font>
@@ -127,9 +137,9 @@ $username = "root";
 $password = "";
 $dbname = "bin";
 
-
     // Open connectivity between PHP and DB
     $conn = new mysqli($servername, $username, $password, $dbname);
+		
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -145,13 +155,13 @@ $dbname = "bin";
 
 	$count = 0;
 
-    //Gets the $result and check if not 0, then interate through the records into the db (id, image, location, date)
+    //Gets the $result and check if not 0, then interate through the records into the db (id, type, image, location, issue, date)
     if ($result->num_rows > 0) {
         // output data of each row
         while($row = $result->fetch_assoc()) {//fecthing each interaction in the DB
 			$count++;
 
-      ///Put int each variable the info taken from the DB for each index number
+      ///Put into each variable the info taken from the DB for each index number
 			$id=$row["id"];
 			$type=$row["type"];
 			$img=$row["image"];
@@ -161,14 +171,14 @@ $dbname = "bin";
 
       //Prints what is in the DB into the page for the specific ID number
 			echo '<td><img src="/images/'.$img.'.png">';
-			echo '<p align="left">Id: '. $id .'</p>';
+			echo '<p align="left">ID: '. $id .'</p>';
 			echo '<p align="left">Type: '. $type .'</p>';
 			echo '<p align="left">Address: '. $location .'</p>';
 			if ($issue)
 					echo '<p align="left"><b>Issues: </b><a style="color:red">'. $issue .'</a></p>';
 				else
 					echo '<p align="left">Issues: none</p>';
-		
+
 			echo '<p align="left">'. $date_variable .'</p></td>';
 
       //Divide the number of bins by 4 to display 4 for each row on the page
@@ -177,8 +187,6 @@ $dbname = "bin";
 				 echo '</tr></div><div class="row"><tr>';
 			  }
         }
-
-
     }
 	else {
         //If there are no bins to be displayed
@@ -189,13 +197,13 @@ $dbname = "bin";
 	}
 	else
 	{
-		echo ('<h1>You\'re not logged as user!</h1>');
+		echo ('<h1>You\'re not logged as a standard user!</h1>');
 		echo '<meta http-equiv="refresh" content="2; URL=/admin/user_registration/logout.php"/>'; //Logs user out when trying to access a not authorized URL
 	}
 }
 else
 {
-	echo ('<h1>You\'re not logged as user!</h1>');
+	echo ('<h1>You\'re not logged as a standard user!</h1>');
 	echo '<meta http-equiv="refresh" content="2; URL=/admin/user_registration/logout.php"/>';
 }
 ?>
@@ -204,11 +212,11 @@ else
 <html>
 	<head>
 		<script type="text/JavaScript">
-    //Function to refresh the page automatically every 28 seconds
+    //Function to refresh the page automatically every 100 seconds
 			function timedRefresh(timeoutPeriod) {
 				setTimeout("location.reload(true);",timeoutPeriod);
 			}
 		</script>
 	</head>
-	<body onload="JavaScript:timedRefresh(128000);"></body>
+	<body onload="JavaScript:timedRefresh(100000);"></body>
 </html>
