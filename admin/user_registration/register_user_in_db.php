4685f550321f6670e08c5835214b377ccd520917
SchmidtRichard <ri.schmidta@gmail.com>
2020-05-08 17:59:27 +0100

    Code tide up and more comments added

--- admin/user_registration/register_user_in_db.php
+++ admin/user_registration/register_user_in_db.php
@@ -5,7 +5,7 @@ $psw = "";
 $dbname = "bin";
 $servername = "localhost";
 
-//info taken from form of user creation in monitoring_admin.php
+//Info taken from form of user creation in monitoring_admin.php
 $name = $_POST['Name'];
 $username = $_POST['Username'];
 $password = $_POST['password1'];
@@ -13,30 +13,30 @@ $password1 = $_POST['password2'];
 $email = $_POST['mail'];
 $admin_rights = $_POST['admin'];
 
-// Connect to DB
+//Connect to DB
 $conn = new mysqli($servername, $usr, $psw, $dbname);
 
 //Send a query to check if that username already exist, before creation.
 $sql = "SELECT * FROM users	WHERE username='$username'";
 $result = $conn->query($sql);
 
-// if there are no results from the above querry, it will continue into creation of the new user
+//If there are no results from the above query, it will continue into creating a new user
 if ($result->num_rows == 0)
 {
-    //we're checking if all those fields has been completed during html form
+    //We're checking if all those fields has been completed on the html form
     if ($name && $username && $password && $password1 && $email && $admin_rights)
     {
-        //this is the step when we check if the email entered is a valid email address e.i. @gmail.com also checks the dot(.)
+        //This is the step when we check if the email entered is a valid email address e.i. @gmail.com also checks the dot(.)
         if (preg_match('/^[0-9A-Za-z\.-_]+@[0-9A-Za-z-]+\.[A-Za-z]{2,4}$/', $email))
         {
             //Check if the password created match
             if ($password == $password1)
             {
-                //insert those info into DB
+                //Insert those info into DB
                 $sql = "INSERT INTO users values('','$name','$username','$password','$email','$admin_rights')";
                 $conn->query($sql);
-                account_created(); //CAlls the function to pop-out the information that user has been created succesfully
-                
+
+                account_created(); //Calls the function to pop-out the information that a new user has been created succesfully
             }
             else
             {
@@ -52,7 +52,7 @@ if ($result->num_rows == 0)
         {
 ?>
 						<script>
-						alert("The email is not completed well!");
+						      alert("The email is not completed well! Please double check it!");
 						</script>
                         <?php
             redirect();
@@ -62,9 +62,10 @@ if ($result->num_rows == 0)
     else
     {
 ?>
-                    <script>
-					alert("Error: All fields should be completed!");
-                    </script>
+          <script>
+					     alert("Error: All fields should be completed!");
+          </script>
+
 					<?php
         redirect();
     }
@@ -72,9 +73,9 @@ if ($result->num_rows == 0)
 else
 {
 ?>
-                    <script>
-					alert("Username already exist! Choose another username!");
-				    </script>
+          <script>
+					     alert("Username already exist! Choose another username!");
+				  </script>
 <?php
     redirect();
 }
@@ -94,6 +95,7 @@ function account_created()
 		</script>
 		</html>
 		<?php
+
     $url = '/admin/monitoring_admin.php';
     echo '<META HTTP-EQUIV=REFRESH CONTENT="0; ' . $url . '">';
 
