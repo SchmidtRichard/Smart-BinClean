74601883677e05ab437febad670ed6f691b63ebe
SchmidtRichard <ri.schmidta@gmail.com>
2020-05-08 17:15:58 +0100

    Code tide up and more comments added

--- admin/user_registration/admin_hide_registration_first_time_only.php
+++ admin/user_registration/admin_hide_registration_first_time_only.php
@@ -1,24 +1,28 @@
 
 <html>
     <head>
-        <title>User registration page</title>
+        <title>User Registration Page</title>
     </head>
 </html>
 <!DOCTYPE html>
 
-<!-- Used when we create the admin for the first time - it is from modal form in the monitoring_admin.php -->
+<!-- Used when we create the admin for the first time -->
 
 <?php
+
 	echo '<html>
 		<body>
 				<div id="Centru">
 					<br><h3 align="center">Admin recording page!</h3>
 					<hr>
-					<p><i>Please complete user data to be registered:</i><p>
+					<p><i>Please Fill in Form:</i><p>
+
+          <!-- post - passes info entered to - rigister_user_in_db.php -->
 					<form action="/admin/user_registration/register_user_in_db.php" method="post">
+
 						Name: <br><input type="text" name="Name">
 					   <br><br>User name:<br><input type="text" name="Username">
-					   <br><br>Passwork:<br><input type="password" name="password1">
+					   <br><br>Password:<br><input type="password" name="password1">
 					   <br>Retype your password:<br><input type="password" name="password2" >
 					   <br><br>Email:<br><input type="text" name="mail" >
              <br>Admin rights?<br>Yes: <input type="checkbox" id="admin" name="admin" value="yes">    No: <input type="checkbox" id="admin" name="admin" value="no">
@@ -28,6 +32,5 @@
 				</div>
 		</body>
 	</html>';
-
 
 ?>
