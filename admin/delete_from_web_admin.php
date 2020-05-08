970622f11b26e2aec3e40362401d31fc34936308
SchmidtRichard <ri.schmidta@gmail.com>
2020-05-08 21:11:47 +0100

    delete_from_web_admin.php - Code tide up and more comments added

--- admin/delete_from_web_admin.php
+++ admin/delete_from_web_admin.php
@@ -1,3 +1,11 @@
+<!--
+Richard Schmidt de Almeida
+National College of Ireland
+Bsc (Honours) in Computing - IoT Stream
+Software Project - May 2020
+Smart BinClean Project
+-->
+
 <?php
 
 //Takes the id parameter from monitoring_admin.php
@@ -9,12 +17,13 @@ $username = "root";
 $password = "";
 $dbname = "bin";
 
-// Create connection
+// Open connectivity between PHP and DB
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-$sql = "SELECT * FROM realtime WHERE id='$id'";	
+//Query if the ID exist in order to be updated or not
+$sql = "SELECT * FROM realtime WHERE id='$id'";
 $result = $conn->query($sql);
-	
+
 if ($result->num_rows != 0)
 {
 
@@ -26,13 +35,13 @@ if ($result->num_rows != 0)
 			echo '<meta http-equiv="refresh" content="2; URL=/admin/monitoring_admin.php"/>';
 			exit;
 		}
-}		
+}
 
 else {
     //echo "Error: " . $sql . "<br>" . $conn->error;
-	  echo "<h1><p style=\"color:red;\">ID does not exist in database!</p></h1>	
+	  echo "<h1><p style=\"color:red;\">ID does not exist in database!</p></h1>
 				Please choose another ID!";
-		echo '<meta http-equiv="refresh" content="3; URL=/admin/monitoring_admin.php"/>';
+		echo '<meta http-equiv="refresh" content="3; URL=/admin/monitoring_admin.php"/>';//Sends back to monitoring_admin.php
 		exit;
 }
 //Close the connection
